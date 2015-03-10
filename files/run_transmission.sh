bd912ff623f1509630f1ee241f7b85e0a246fdf5
Ben Koerbel <bkoerbel@gmail.com>
2015-03-10 00:27:54 +0000

    adding no authentication option

--- files/run_transmission.sh
+++ files/run_transmission.sh
@@ -7,12 +7,16 @@ SETTINGS=$CONFIG_DIR/settings.json
 TRANSMISSION=/usr/bin/transmission-daemon
 
 if [[ ! -f ${SETTINGS}.bak ]]; then
-    if [ -z $ADMIN_PASS ]; then
+    if [[ $NO_AUTH == TRUE ]]; then
+        sed -i.bak -e "s/@authentication@/false/" $SETTINGS
+    elif [ -z $ADMIN_PASS ]; then
         echo Please provide a password for the 'transmission' user via the ADMIN_PASS enviroment variable.
         exit 1
+    else
+        sed -i.bak -e "s/@authentication@/true/" $SETTINGS
+        sed $SETTINGS -e "s/@password@/$ADMIN_PASS/" $SETTINGS
     fi
 
-    sed -i.bak -e "s/@password@/$ADMIN_PASS/" $SETTINGS 
 fi
 
 unset TRANSMISION_ADMIN_PASS
